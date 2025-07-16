953f0e32d59b99e74220bedcbba3a00037c770a7
huiae07 <huiae07@gmail.com>
2025-07-16 16:18:51 +0900

    feat: cors 추가

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // 또는 true로 하면 모든 도메인 허용
+    origin: ['http://localhost:3000', 'https://toolbox-vod.vercel.app'], // 또는 true로 하면 모든 도메인 허용
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3100);
